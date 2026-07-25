a1659e19cca933bcf8afbb41488bc94924ce3add
Ariya <ariya23jg@gmail.com>
2026-07-25 23:21:45 +0530

    Keep ONLY democriticIntelligence in repo root on demo branch

--- coral_wemakedevs/incident-time-machine/sql/catalog.sql
+++
@@ -1,5 +0,0 @@
-SELECT schema_name, table_name
-FROM coral.tables
-WHERE schema_name LIKE '%_demo'
-ORDER BY schema_name, table_name;
-
--- coral_wemakedevs/incident-time-machine/sql/columns.sql
+++
@@ -1,5 +0,0 @@
-SELECT schema_name, table_name, column_name, data_type
-FROM coral.columns
-WHERE schema_name LIKE '%_demo'
-ORDER BY schema_name, table_name, ordinal_position;
-
